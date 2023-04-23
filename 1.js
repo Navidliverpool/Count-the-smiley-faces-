8a2a59457db9105776e5c7df37513c3e4e1fc013
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-23 16:23:25 +0100

    1.js added.

---
+++ 1.js
@@ -0,0 +1,26 @@
+//  This method works but what it does is different than what the instruction of this pseudocode is asking.
+function countSmileys(arr) {
+    let s = '';
+    let l = ')';
+    // let ll = '-)';
+    // let lll = '~)';
+    let D = 'D';
+    // let DD = '-D';
+    // let DDD = '~D';
+    let a = 0;
+    for (let i = 0; i < arr.length; i++) {
+        console.log("i:", i)
+        s += arr[i].slice(-1);
+        console.log("s:", s)
+    }
+    let ss = s.split('');
+    console.log("ss:", ss)
+    for (let t of ss) {
+        if (t == l || t == D) {
+            a++;
+        }
+    }
+    return a;
+}
+let array = ([':-)', ';~D', ':-D', ':_D']);
+console.log(countSmileys(array))
